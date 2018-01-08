e973fb8e73f4df92593066def0558867bcda20d8
shivam123425 <shivam123425@gmail.com>
2018-01-08 17:09:22 +0530

    Todo-Added Font Awesome and also the animated hover Trash Icon

--- Todo /assets/js/todos.js
+++ Todo /assets/js/todos.js
@@ -16,7 +16,11 @@ $("input[type='text']").keypress(function(event) {
 	if (event.which===13) {
 		//  New Todo
 		var newTodo=$(this).val();
-		$("ul").append('<li><span>X</span> '+newTodo+'</li>');
+		$("ul").append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> '+newTodo+'</li>');
 		$(this).val("");
 	}
+});
+
+$("i").on("click",function() {
+	$("input[type='text']").fadeToggle(500);
 });
